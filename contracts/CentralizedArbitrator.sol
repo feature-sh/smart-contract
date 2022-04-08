fece4b7ce29e7d996d233b3d0f83e714cdae342b
Opixelum <12w0zvkgazd3@opayq.net>
2022-04-08 14:43:09 +0200

    Refactored contract

--- contracts/CentralizedArbitrator.sol
+++ contracts/CentralizedArbitrator.sol
@@ -25,7 +25,10 @@ abstract contract Arbitrable is IArbitrable {
     Arbitrator public arbitrator;
     bytes public arbitratorExtraData; // Extra data to require particular dispute and appeal behaviour.
 
-    modifier onlyArbitrator {require(msg.sender == address(arbitrator), "Can only be called by the arbitrator."); _;}
+    modifier onlyArbitrator() {
+        require(msg.sender == address(arbitrator), "Can only be called by the arbitrator.");
+        _;
+    }
 
     /** @dev Constructor. Choose the arbitrator.
      *  @param _arbitrator The arbitrator of the contract.
@@ -41,67 +44,67 @@ abstract contract Arbitrable is IArbitrable {
      *  @param _disputeID ID of the dispute in the Arbitrator contract.
      *  @param _ruling Ruling given by the arbitrator. Note that 0 is reserved for "Not able/wanting to make a decision".
      */
-    function rule(uint _disputeID, uint _ruling) public override onlyArbitrator {
-        emit Ruling(Arbitrator(msg.sender),_disputeID,_ruling);
+    function rule(uint256 _disputeID, uint256 _ruling) public override onlyArbitrator {
+        emit Ruling(Arbitrator(msg.sender), _disputeID, _ruling);
 
-        executeRuling(_disputeID,_ruling);
+        executeRuling(_disputeID, _ruling);
     }
-
 
     /** @dev Execute a ruling of a dispute.
      *  @param _disputeID ID of the dispute in the Arbitrator contract.
      *  @param _ruling Ruling given by the arbitrator. Note that 0 is reserved for "Not able/wanting to make a decision".
      */
-    function executeRuling(uint _disputeID, uint _ruling) internal virtual;
+    function executeRuling(uint256 _disputeID, uint256 _ruling) internal virtual;
 }
-
 
 /** @title Centralized Arbitrator
  *  @dev This is a centralized arbitrator deciding alone on the result of disputes. No appeals are possible.
  */
 abstract contract CentralizedArbitrator is Arbitrator {
-
     address public owner = msg.sender;
-    uint arbitrationPrice; // Not public because arbitrationCost already acts as an accessor.
-    uint constant NOT_PAYABLE_VALUE = (2**256-2)/2; // High value to be sure that the appeal is too expensive.
+    uint256 arbitrationPrice; // Not public because arbitrationCost already acts as an accessor.
+    uint256 constant NOT_PAYABLE_VALUE = (2**256 - 2) / 2; // High value to be sure that the appeal is too expensive.
 
     struct DisputeStruct {
         Arbitrable arbitrated;
-        uint choices;
-        uint fee;
-        uint ruling;
+        uint256 choices;
+        uint256 fee;
+        uint256 ruling;
         DisputeStatus status;
     }
 
-    modifier onlyOwner {require(msg.sender==owner, "Can only be called by the owner."); _;}
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Can only be called by the owner.");
+        _;
+    }
 
     DisputeStruct[] public disputes;
 
     /** @dev Constructor. Set the initial arbitration price.
      *  @param _arbitrationPrice Amount to be paid for arbitration.
      */
-    constructor(uint _arbitrationPrice) {
+    constructor(uint256 _arbitrationPrice) {
         arbitrationPrice = _arbitrationPrice;
     }
 
     /** @dev Set the arbitration price. Only callable by the owner.
      *  @param _arbitrationPrice Amount to be paid for arbitration.
      */
-    function setArbitrationPrice(uint _arbitrationPrice) public onlyOwner {
+    function setArbitrationPrice(uint256 _arbitrationPrice) public onlyOwner {
         arbitrationPrice = _arbitrationPrice;
     }
 
     /** @dev Cost of arbitration. Accessor to arbitrationPrice.
      *  @return fee Amount to be paid.
      */
-    function arbitrationCost() public view returns(uint fee) {
+    function arbitrationCost() public view returns (uint256 fee) {
         return arbitrationPrice;
     }
 
     /** @dev Cost of appeal. Since it is not possible, it's a high value which can never be paid.
      *  @return fee Amount to be paid.
      */
-    function appealCost() public pure returns(uint fee) {
+    function appealCost() public pure returns (uint256 fee) {
         return NOT_PAYABLE_VALUE;
     }
 
@@ -111,15 +114,22 @@ abstract contract CentralizedArbitrator is Arbitrator {
      *  @param _extraData Can be used to give additional info on the dispute to be created.
      *  @return disputeID ID of the dispute created.
      */
-    function createDispute(uint _choices, bytes calldata _extraData) public payable override returns(uint disputeID)  {
+    function createDispute(uint256 _choices, bytes calldata _extraData)
+        public
+        payable
+        override
+        returns (uint256 disputeID)
+    {
         super.createDispute(_choices, _extraData);
-        disputes.push(DisputeStruct({
-            arbitrated: Arbitrable(msg.sender),
-            choices: _choices,
-            fee: msg.value,
-            ruling: 0,
-            status: DisputeStatus.Waiting
-        }));
+        disputes.push(
+            DisputeStruct({
+                arbitrated: Arbitrable(msg.sender),
+                choices: _choices,
+                fee: msg.value,
+                ruling: 0,
+                status: DisputeStatus.Waiting
+            })
+        );
         disputeID = disputes.length - 1;
         emit DisputeCreation(disputeID, Arbitrable(msg.sender));
     }
@@ -128,7 +138,7 @@ abstract contract CentralizedArbitrator is Arbitrator {
      *  @param _disputeID ID of the dispute to rule.
      *  @param _ruling Ruling given by the arbitrator. Note that 0 means "Not able/wanting to make a decision".
      */
-    function _giveRuling(uint _disputeID, uint _ruling) internal {
+    function _giveRuling(uint256 _disputeID, uint256 _ruling) internal {
         DisputeStruct storage dispute = disputes[_disputeID];
         require(_ruling <= dispute.choices, "Invalid ruling.");
         require(dispute.status != DisputeStatus.Solved, "The dispute must not be solved already.");
@@ -137,14 +147,14 @@ abstract contract CentralizedArbitrator is Arbitrator {
         dispute.status = DisputeStatus.Solved;
 
         payable(msg.sender).transfer(dispute.fee); // Avoid blocking.
-        dispute.arbitrated.rule(_disputeID,_ruling);
+        dispute.arbitrated.rule(_disputeID, _ruling);
     }
 
     /** @dev Give a ruling. UNTRUSTED.
      *  @param _disputeID ID of the dispute to rule.
      *  @param _ruling Ruling given by the arbitrator. Note that 0 means "Not able/wanting to make a decision".
      */
-    function giveRuling(uint _disputeID, uint _ruling) public onlyOwner {
+    function giveRuling(uint256 _disputeID, uint256 _ruling) public onlyOwner {
         return _giveRuling(_disputeID, _ruling);
     }
 
@@ -152,7 +162,7 @@ abstract contract CentralizedArbitrator is Arbitrator {
      *  @param _disputeID ID of the dispute to rule.
      *  @return status The status of the dispute.
      */
-    function disputeStatus(uint _disputeID) public view override returns(DisputeStatus status) {
+    function disputeStatus(uint256 _disputeID) public view override returns (DisputeStatus status) {
         return disputes[_disputeID].status;
     }
 
@@ -160,7 +170,7 @@ abstract contract CentralizedArbitrator is Arbitrator {
      *  @param _disputeID ID of the dispute to rule.
      *  @return ruling The ruling which would or has been given.
      */
-    function currentRuling(uint _disputeID) public view override returns(uint ruling) {
+    function currentRuling(uint256 _disputeID) public view override returns (uint256 ruling) {
         return disputes[_disputeID].ruling;
     }
 }
