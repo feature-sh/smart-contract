d683dfff40a451ec8f2b212468e20fac862e0667
Opixelum <12w0zvkgazd3@opayq.net>
2022-04-11 12:14:00 +0200

    copy existing arbitrator contract with some changes for erc20

---
+++ contracts/CentralizedArbitratorERC20.sol
@@ -0,0 +1,185 @@
+/**
+ *  @authors: [@clesaege, @n1c01a5, @epiqueras, @ferittuncer, @Opixelum]
+ *  @reviewers: [@clesaege*, @unknownunknown1*]
+ *  @auditors: []
+ *  @bounties: []
+ *  @deployments: []
+ *  @tools: [MythX]
+ */
+
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.7;
+
+import "./CentralizedAppealableArbitrator.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+/** @title Arbitrable
+ *  @author Clément Lesaege - <clement@lesaege.com>
+ *  Arbitrable abstract contract.
+ *  When developing arbitrable contracts, we need to:
+ *  -Define the action taken when a ruling is received by the contract. We should do so in executeRuling.
+ *  -Allow dispute creation. For this a function must:
+ *      -Call arbitrator.createDispute.value(_fee)(_choices,_extraData);
+ *      -Create the event Dispute(_arbitrator,_disputeID,_rulingOptions);
+ */
+abstract contract Arbitrable is IArbitrable {
+    Arbitrator public arbitrator;
+    bytes public arbitratorExtraData; // Extra data to require particular dispute and appeal behaviour.
+
+    modifier onlyArbitrator() {
+        require(msg.sender == address(arbitrator), "Can only be called by the arbitrator.");
+        _;
+    }
+
+    /** @dev Constructor. Choose the arbitrator.
+     *  @param _arbitrator The arbitrator of the contract.
+     *  @param _arbitratorExtraData Extra data for the arbitrator.
+     */
+    constructor(Arbitrator _arbitrator, bytes memory _arbitratorExtraData) {
+        arbitrator = _arbitrator;
+        arbitratorExtraData = _arbitratorExtraData;
+    }
+
+    /** @dev Give a ruling for a dispute. Must be called by the arbitrator.
+     *  The purpose of this function is to ensure that the address calling it has the right to rule on the contract.
+     *  @param _disputeID ID of the dispute in the Arbitrator contract.
+     *  @param _ruling Ruling given by the arbitrator. Note that 0 is reserved for "Not able/wanting to make a decision".
+     */
+    function rule(uint256 _disputeID, uint256 _ruling) public override onlyArbitrator {
+        emit Ruling(Arbitrator(msg.sender), _disputeID, _ruling);
+
+        executeRuling(_disputeID, _ruling);
+    }
+
+    /** @dev Execute a ruling of a dispute.
+     *  @param _disputeID ID of the dispute in the Arbitrator contract.
+     *  @param _ruling Ruling given by the arbitrator. Note that 0 is reserved for "Not able/wanting to make a decision".
+     */
+    function executeRuling(uint256 _disputeID, uint256 _ruling) internal virtual;
+}
+
+/** @title Centralized Arbitrator
+ *  @dev This is a centralized arbitrator deciding alone on the result of disputes. No appeals are possible.
+ */
+abstract contract CentralizedArbitratorERC20 is Arbitrator {
+    address public owner = msg.sender;
+    uint256 arbitrationPrice; // Not public because arbitrationCost already acts as an accessor.
+    uint256 constant NOT_PAYABLE_VALUE = (2**256 - 2) / 2; // High value to be sure that the appeal is too expensive.
+
+    struct DisputeStruct {
+        Arbitrable arbitrated;
+        uint256 choices;
+        IERC20 token;
+        uint256 fee;
+        uint256 ruling;
+        DisputeStatus status;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Can only be called by the owner.");
+        _;
+    }
+
+    DisputeStruct[] public disputes;
+
+    /** @dev Constructor. Set the initial arbitration price.
+     *  @param _arbitrationPrice Amount to be paid for arbitration.
+     */
+    constructor(uint256 _arbitrationPrice) {
+        arbitrationPrice = _arbitrationPrice;
+    }
+
+    /** @dev Set the arbitration price. Only callable by the owner.
+     *  @param _arbitrationPrice Amount to be paid for arbitration.
+     */
+    function setArbitrationPrice(uint256 _arbitrationPrice) public onlyOwner {
+        arbitrationPrice = _arbitrationPrice;
+    }
+
+    /** @dev Cost of arbitration. Accessor to arbitrationPrice.
+     *  @return fee Amount to be paid.
+     */
+    function arbitrationCost() public view returns (uint256 fee) {
+        return arbitrationPrice;
+    }
+
+    /** @dev Cost of appeal. Since it is not possible, it's a high value which can never be paid.
+     *  @return fee Amount to be paid.
+     */
+    function appealCost() public pure returns (uint256 fee) {
+        return NOT_PAYABLE_VALUE;
+    }
+
+    /** @dev Create a dispute. Must be called by the arbitrable contract.
+     *  Must be paid at least arbitrationCost().
+     *  @param _choices Amount of choices the arbitrator can make in this dispute. When ruling ruling<=choices.
+     *  @param _token Token to be used for the arbitration.
+     *  @param _fee Amount to be paid for arbitration.
+     *  @param _extraData Can be used to give additional info on the dispute to be created.
+     *  @return disputeID ID of the dispute created.
+     */
+    function createDispute(uint256 _choices, IERC20 _token, uint256 _fee, bytes calldata _extraData)
+        public
+        payable
+        returns (uint256 disputeID)
+    {
+        // Transfers token from sender wallet to contract.
+        require(
+            _token.transferFrom(msg.sender, address(this), _fee),
+            "Sender does not have enough approved funds."
+        );
+        super.createDispute(_choices, _extraData);
+        disputes.push(
+            DisputeStruct({
+                arbitrated: Arbitrable(msg.sender),
+                choices: _choices,
+                token: _token,
+                fee: _fee,
+                ruling: 0,
+                status: DisputeStatus.Waiting
+            })
+        );
+        disputeID = disputes.length - 1;
+        emit DisputeCreation(disputeID, Arbitrable(msg.sender));
+    }
+
+    /** @dev Give a ruling. UNTRUSTED.
+     *  @param _disputeID ID of the dispute to rule.
+     *  @param _ruling Ruling given by the arbitrator. Note that 0 means "Not able/wanting to make a decision".
+     */
+    function _giveRuling(uint256 _disputeID, uint256 _ruling) internal {
+        DisputeStruct storage dispute = disputes[_disputeID];
+        require(_ruling <= dispute.choices, "Invalid ruling.");
+        require(dispute.status != DisputeStatus.Solved, "The dispute must not be solved already.");
+
+        dispute.ruling = _ruling;
+        dispute.status = DisputeStatus.Solved;
+
+        IERC20(dispute.token).transfer(address(this), dispute.fee);
+        dispute.arbitrated.rule(_disputeID, _ruling);
+    }
+
+    /** @dev Give a ruling. UNTRUSTED.
+     *  @param _disputeID ID of the dispute to rule.
+     *  @param _ruling Ruling given by the arbitrator. Note that 0 means "Not able/wanting to make a decision".
+     */
+    function giveRuling(uint256 _disputeID, uint256 _ruling) public onlyOwner {
+        return _giveRuling(_disputeID, _ruling);
+    }
+
+    /** @dev Return the status of a dispute.
+     *  @param _disputeID ID of the dispute to rule.
+     *  @return status The status of the dispute.
+     */
+    function disputeStatus(uint256 _disputeID) public view override returns (DisputeStatus status) {
+        return disputes[_disputeID].status;
+    }
+
+    /** @dev Return the ruling of a dispute.
+     *  @param _disputeID ID of the dispute to rule.
+     *  @return ruling The ruling which would or has been given.
+     */
+    function currentRuling(uint256 _disputeID) public view override returns (uint256 ruling) {
+        return disputes[_disputeID].ruling;
+    }
+}
