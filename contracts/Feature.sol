0849142f4f9721b7731c6d02bbb6fe1ba5a2fde8
Nicolas Wagner <contact@wagner-nicolas.com>
2023-11-01 17:07:56 +0100

    fix: wording and gas optimization for ERC20 contract

--- contracts/Feature.sol
+++ contracts/Feature.sol
@@ -199,7 +199,7 @@ contract NativeMetaTransaction is EIP712Base {
         return returnData;
     }
 
-    function hashMetaTransaction(MetaTransaction memory metaTx) internal pure returns (bytes32) {
+    function hashMetaTransaction(MetaTransaction memory metaTx) internal view returns (bytes32) {
         return
             keccak256(
                 abi.encode(META_TRANSACTION_TYPEHASH, metaTx.nonce, metaTx.from, keccak256(metaTx.functionSignature))
